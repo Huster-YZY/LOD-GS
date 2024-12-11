5814d2b1e21bc99ab5b657e888d05fa324857a83
Huster-YZY <2928664165@qq.com>
2024-12-11 22:48:29 +0800

    update

--- metrics.py
+++ metrics.py
@@ -101,6 +101,103 @@ def evaluate(model_paths):
         except:
             print("Unable to compute metrics for model", scene_dir)
 
+def evaluate_res(model_paths, mid_point):
+
+    full_dict = {}
+    per_view_dict = {}
+    full_dict_polytopeonly = {}
+    per_view_dict_polytopeonly = {}
+    print("")
+
+    for scene_dir in model_paths:
+        try:
+            print("Scene:", scene_dir)
+            full_dict[scene_dir] = {}
+            per_view_dict[scene_dir] = {}
+            full_dict_polytopeonly[scene_dir] = {}
+            per_view_dict_polytopeonly[scene_dir] = {}
+
+            test_dir = Path(scene_dir) / "test"
+
+            for method in os.listdir(test_dir):
+                print("Method:", method)
+
+                full_dict[scene_dir][method] = {}
+                per_view_dict[scene_dir][method] = {}
+                full_dict_polytopeonly[scene_dir][method] = {}
+                per_view_dict_polytopeonly[scene_dir][method] = {}
+
+                method_dir = test_dir / method
+                gt_dir = method_dir/ "gt"
+                renders_dir = method_dir / "renders"
+                masks_dir = method_dir / "masks"
+                renders, gts, masks, image_names = readImages(renders_dir, gt_dir, masks_dir)
+
+                ssims = []
+                psnrs = []
+                lpipss = []
+                psnr_masks = []
+
+                for idx in tqdm(range(mid_point), desc="Metric evaluation progress"):
+                    ssims.append(ssim(renders[idx], gts[idx]))
+                    psnrs.append(psnr(renders[idx], gts[idx]))
+                    lpipss.append(lpips(renders[idx], gts[idx], net_type='vgg'))
+                    psnr_masks.append(psnr_mask(renders[idx],gts[idx],masks[idx]))
+
+                print("  SSIM : {:>12.7f}".format(torch.tensor(ssims).mean(), ".5"))
+                print("  PSNR : {:>12.7f}".format(torch.tensor(psnrs).mean(), ".5"))
+                print("  Masked PSNR: {:>12.7f}".format(torch.tensor(psnr_masks).mean(), ".5"))
+                print("  LPIPS: {:>12.7f}".format(torch.tensor(lpipss).mean(), ".5"))
+                print("")
+
+                full_dict[scene_dir][method].update({"SSIM": torch.tensor(ssims).mean().item(),
+                                                        "PSNR": torch.tensor(psnrs).mean().item(),
+                                                        "Masked PSNR": torch.tensor(psnr_masks).mean().item(),
+                                                        "LPIPS": torch.tensor(lpipss).mean().item()})
+                per_view_dict[scene_dir][method].update({"SSIM": {name: ssim for ssim, name in zip(torch.tensor(ssims).tolist(), image_names)},
+                                                            "PSNR": {name: psnr for psnr, name in zip(torch.tensor(psnrs).tolist(), image_names)},
+                                                            "Masked PSNR": {name: psnr for psnr, name in zip(torch.tensor(psnr_masks).tolist(), image_names)},
+                                                            "LPIPS": {name: lp for lp, name in zip(torch.tensor(lpipss).tolist(), image_names)}})
+
+                with open(scene_dir + "/results_coarse.json", 'w') as fp:
+                    json.dump(full_dict[scene_dir], fp, indent=True)
+                with open(scene_dir + "/per_view_coarse.json", 'w') as fp:
+                    json.dump(per_view_dict[scene_dir], fp, indent=True)
+
+
+                ssims = []
+                psnrs = []
+                lpipss = []
+                psnr_masks = []
+
+                for idx in tqdm(range(mid_point, len(renders)), desc="Metric evaluation progress"):
+                    ssims.append(ssim(renders[idx], gts[idx]))
+                    psnrs.append(psnr(renders[idx], gts[idx]))
+                    lpipss.append(lpips(renders[idx], gts[idx], net_type='vgg'))
+                    psnr_masks.append(psnr_mask(renders[idx],gts[idx],masks[idx]))
+
+                print("  SSIM : {:>12.7f}".format(torch.tensor(ssims).mean(), ".5"))
+                print("  PSNR : {:>12.7f}".format(torch.tensor(psnrs).mean(), ".5"))
+                print("  Masked PSNR: {:>12.7f}".format(torch.tensor(psnr_masks).mean(), ".5"))
+                print("  LPIPS: {:>12.7f}".format(torch.tensor(lpipss).mean(), ".5"))
+                print("")
+
+                full_dict[scene_dir][method].update({"SSIM": torch.tensor(ssims).mean().item(),
+                                                        "PSNR": torch.tensor(psnrs).mean().item(),
+                                                        "Masked PSNR": torch.tensor(psnr_masks).mean().item(),
+                                                        "LPIPS": torch.tensor(lpipss).mean().item()})
+                per_view_dict[scene_dir][method].update({"SSIM": {name: ssim for ssim, name in zip(torch.tensor(ssims).tolist(), image_names)},
+                                                            "PSNR": {name: psnr for psnr, name in zip(torch.tensor(psnrs).tolist(), image_names)},
+                                                            "Masked PSNR": {name: psnr for psnr, name in zip(torch.tensor(psnr_masks).tolist(), image_names)},
+                                                            "LPIPS": {name: lp for lp, name in zip(torch.tensor(lpipss).tolist(), image_names)}})
+
+                with open(scene_dir + "/results_fine.json", 'w') as fp:
+                    json.dump(full_dict[scene_dir], fp, indent=True)
+                with open(scene_dir + "/per_view_fine.json", 'w') as fp:
+                    json.dump(per_view_dict[scene_dir], fp, indent=True)
+        except:
+            print("Unable to compute metrics for model", scene_dir)
+
 if __name__ == "__main__":
     device = torch.device("cuda:0")
     torch.cuda.set_device(device)
@@ -109,4 +206,5 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Training script parameters")
     parser.add_argument('--model_paths', '-m', required=True, nargs="+", type=str, default=[])
     args = parser.parse_args()
-    evaluate(args.model_paths)
+    # evaluate(args.model_paths)
+    evaluate_res(args.model_paths, 20)
